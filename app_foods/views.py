3125fb69df07845c002d55fdd23037146198e9ca
SkywalkerGK <lanend18@gmail.com>
2023-02-23 22:03:29 +0700

    create_app_foods

--- app_foods/views.py
+++ app_foods/views.py
@@ -1,3 +1,7 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def foods(request):
+    return HttpResponse('เมนู อร่อย ส่งบ่อย ส่งไว')
+
--- app_general/views.py
+++ app_general/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return HttpResponse('จันทร์ไรเดอร์')
+
+def about(request):
+    return HttpResponse('เกี่ยวกับเรา')
