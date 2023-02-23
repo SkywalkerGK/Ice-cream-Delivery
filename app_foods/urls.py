432fd89a233cc35a637b6b45071224158a90970a
SkywalkerGK <lanend18@gmail.com>
2023-02-23 22:12:00 +0700

    Food_id

--- app_foods/urls.py
+++ app_foods/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.foods, name='foods'),
+    path('',views.foods, name='foods'), #foods/
+    path('<int:food_id>',views.food, name='food') #foods/1 2 3
 
 
 ]
--- app_foods/views.py
+++ app_foods/views.py
@@ -5,3 +5,6 @@ from django.shortcuts import render
 def foods(request):
     return HttpResponse('เมนู อร่อย ส่งบ่อย ส่งไว')
 
+def food(request,food_id):
+    return HttpResponse('เมนูนี้ ID = ' + str(food_id))
+
