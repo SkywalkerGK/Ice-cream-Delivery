715772b1a4c64e13aaa5591a5e2cd3f38878116f
SkywalkerGK <lanend18@gmail.com>
2023-02-26 12:11:55 +0700

    Tag_Foods

--- app_foods/views.py
+++ app_foods/views.py
@@ -1,9 +1,18 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
 
+
+all_foods = [
+    {'id':1,'title':'Dark Choco Premium','price':499,'is_premium':True},
+    {'id':2,'title':'Red Spicy','price':349,'is_premium':False},
+    {'id':3,'title':'Blue Glacier','price':349,'is_premium':False},
+]
+
+
 # Create your views here.
 def foods(request):
-    return render(request,'app_foods/foods.html')
+    context = {'foods':all_foods}
+    return render(request,'app_foods/foods.html',context)
 
 def food(request,food_id):
     return render(request,'app_foods/food.html', context={'food_id': food_id})
