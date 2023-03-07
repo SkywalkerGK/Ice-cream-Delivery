49503e9c1f53a5a31dcfdae00fe4e4b2625a7431
SkywalkerGK <lanend18@gmail.com>
2023-03-08 01:31:27 +0700

    Creat_Scription

--- app_general/models.py
+++ app_general/models.py
@@ -13,3 +13,4 @@ class Subsciption(models.Model):
     email = models.EmailField(max_length=60, unique=True)
     status = models.CharField(max_length=15, choices=STATUS, default='unapproved')
     registered_at = models.DateTimeField(auto_now_add=True)
+    food_set = models.ManyToManyField('app_foods.Food')
