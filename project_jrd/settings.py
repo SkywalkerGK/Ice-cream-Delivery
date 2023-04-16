2b60e1f0b08f4011083e21779ecf8816fafb27ad
Namcha <Lanend18@gmail.com>
2023-04-16 16:46:02 +0700

    Change-Password

--- project_jrd/settings.py
+++ project_jrd/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'app_user.apps.AppUserConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -41,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'app_general.apps.AppGeneralConfig',
     'app_foods.apps.AppFoodsConfig',
-    'app_user.apps.AppUserConfig',
+    
     
 ]
 
