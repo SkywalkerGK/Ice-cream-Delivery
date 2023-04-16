9cb284e140f8e0d410ed34c0e0dbc195a4972819
Namcha <Lanend18@gmail.com>
2023-04-16 15:02:05 +0700

    Save-Profile

--- app_user/forms.py
+++ app_user/forms.py
@@ -1,5 +1,18 @@
 from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.contrib.auth.models import User
+from app_user.models import Profile
 
 class RegisterForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         fields = UserCreationForm.Meta.fields+("email",)
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ("first_name","last_name")
+
+class ExtendedProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ("address","phone") 
--- app_user/views.py
+++ app_user/views.py
@@ -2,8 +2,10 @@ from django.http import HttpRequest, HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
-from app_user.forms import RegisterForm
+from app_user.forms import RegisterForm,UserProfileForm,ExtendedProfileForm
 from django.contrib.auth import login
+
+
 # Create your views here.
 
 def register(request: HttpRequest):
@@ -25,3 +27,39 @@ def register(request: HttpRequest):
 def dashboard(request: HttpRequest):
     return render(request, "app_users/dashboard.html")
 
+@login_required
+def profile(request: HttpRequest):
+    user = request.user
+    #POST
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=user) #Saveแบบ USER
+        is_new_profile = False #User มี Profile
+
+        try:  #พยายามอัพเดตดูก่อน
+            extended_form = ExtendedProfileForm(request.POST, instance=user.profile)
+        except: #ถ้าไม่ได้จะสร้างโปรไฟล์อันใหม่
+            extended_form = ExtendedProfileForm(request.POST)  
+            is_new_profile = True   #User ไม่มี Profile
+
+        if form.is_valid() and extended_form.is_valid():
+            form.save()
+            if is_new_profile: #สร้างใหม่
+                profile = extended_form.save(commit=False)
+                profile.user = user
+                profile.save()
+            else:  #เซฟเลย / อัพเดต
+                extended_form.save()    
+
+
+            return HttpResponseRedirect(reverse("profile"))
+    else:
+        form = UserProfileForm()
+        extended_form = ExtendedProfileForm()
+
+    #GET
+    context = {
+        "form": form,
+        "extended_form": extended_form
+    }
+    return render(request, "app_users/profile.html" , context)
+    
